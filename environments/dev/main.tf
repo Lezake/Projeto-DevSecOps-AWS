c759eb668a3afa438cc28bde3f0c860a223ec86e
Lezake <lezakee@proton.me>
2026-08-24 14:01:06 -0300

    ci: add github actions workflow with oidc and checkov

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,6 @@
 module "network" {
   source = "../../modules/network"
-  
+
   vpc_cidr    = "10.0.0.0/16"
   subnet_cidr = "10.0.1.0/24"
 }
--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -3,9 +3,9 @@ terraform {
 
   # AQUI ESTÁ O NOVO BLOCO
   backend "s3" {
-    bucket       = "lezake-tfstate-devsecops-908027385183"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-1"
+    bucket         = "lezake-tfstate-devsecops-908027385183"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
     use_path_style = true # Recomendação atual para S3
     # NOTA CORPORATIVA: Não usamos 'dynamodb_table' aqui pois o Terraform 1.10+
     # gerencia o locking nativamente no S3 usando APIs de condicional PUT.
