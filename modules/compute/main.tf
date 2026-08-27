b2165e145c70e93e3c1b1e3e00edb2540118fc03
Lezake <lezakee@proton.me>
2026-08-27 20:52:00 -0300

    refactor: harden iac security and adhere to standard module structure

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -12,10 +12,18 @@ resource "aws_security_group" "web" {
   }
 
   egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "Allow outbound HTTP for package downloads"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow outbound HTTPS for package downloads"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -37,6 +45,12 @@ resource "aws_launch_template" "web" {
 
   vpc_security_group_ids = [aws_security_group.web.id]
   user_data              = filebase64("${path.module}/user_data.sh")
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
 
   tag_specifications {
     resource_type = "instance"
